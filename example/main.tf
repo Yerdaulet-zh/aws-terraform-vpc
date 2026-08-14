1d20bc2a63534d9240a1d859d5565acf39b46e07
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-08-14 18:30:09 +0500

    KAN-1: add example .tf files which uses the VPC module

---
+++ example/main.tf
@@ -0,0 +1,18 @@
+module "vpc_test" {
+  source = "../"
+
+  vpc_config = {
+    ipam_ipv4_id                         = null
+    ipv4_netmask_length                  = null
+    ipv4_cidr_block                      = "10.100.0.0/16"
+    assign_generated_ipv6_cidr_block     = true
+    instance_tenancy                     = "default"
+    enable_dns_support                   = true
+    enable_network_address_usage_metrics = true
+    enable_dns_hostnames                 = true
+    tags = {
+      Owner     = "DevOpsTeam"
+      ManagedBy = "Terraform"
+    }
+  }
+}
