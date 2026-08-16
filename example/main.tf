bca394bb963fc931a88e6775f7f1cdd651f6270d
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-08-16 17:06:12 +0500

    KAN-2: set global variables in example.main.tf file

--- example/main.tf
+++ example/main.tf
@@ -1,13 +1,13 @@
 module "vpc_test" {
   source = "../"
 
+  global = {
+    project_name = "vpc-module-example"
+    environment  = "dev"
+  }
+
   vpc_config = {
     ipv4_cidr_block                  = "10.100.0.0/16"
     assign_generated_ipv6_cidr_block = true
-    tags = {
-      Name      = "vpc-module-example"
-      Owner     = "DevOpsTeam"
-      ManagedBy = "Terraform"
-    }
   }
 }
