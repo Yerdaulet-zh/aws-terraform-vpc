7b95ee011df35ee98b950756245123ae1c6b7260
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-08-16 15:32:55 +0500

    KAN-1: use optional in VPC module's variables.tf file

--- variables.tf
+++ variables.tf
@@ -1,31 +1,17 @@
 variable "vpc_config" {
   description = "Default configuration for the VPC"
   type = object({
-    ipv4_ipam_id                         = string
-    ipv4_netmask_length                  = number
-    ipv4_cidr_block                      = string
-    assign_generated_ipv6_cidr_block     = bool
-    instance_tenancy                     = string
-    enable_dns_support                   = bool
-    enable_network_address_usage_metrics = bool
-    enable_dns_hostnames                 = bool
-    tags = object({
-      Owner     = string
-      ManagedBy = string
-    })
-  })
-  default = {
-    ipv4_ipam_id                         = null,
-    ipv4_netmask_length                  = null,
-    ipv4_cidr_block                      = "10.0.0.0/16",
-    assign_generated_ipv6_cidr_block     = true
-    instance_tenancy                     = "default"
-    enable_dns_support                   = true
-    enable_network_address_usage_metrics = true
-    enable_dns_hostnames                 = true
-    tags = {
+    ipv4_ipam_id                         = optional(string)
+    ipv4_netmask_length                  = optional(number)
+    ipv4_cidr_block                      = optional(string, "10.0.0.0/16")
+    assign_generated_ipv6_cidr_block     = optional(bool, true)
+    instance_tenancy                     = optional(string, "default")
+    enable_dns_support                   = optional(bool, true)
+    enable_network_address_usage_metrics = optional(bool, true)
+    enable_dns_hostnames                 = optional(bool, true)
+    tags = optional(map(string), {
       Owner     = "Terraform"
       ManagedBy = "Terraform"
-    }
-  }
+    })
+  })
 }
