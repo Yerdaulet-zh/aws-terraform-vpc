2675250a569630d1a011fba2986188aac776bac6
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-08-16 15:49:47 +0500

    KAN-1: set the example VPC name

--- example/main.tf
+++ example/main.tf
@@ -4,6 +4,7 @@ module "vpc_test" {
   vpc_config = {
     ipv4_cidr_block = "10.100.0.0/16"
     tags = {
+      Name      = "vpc-module-example"
       Owner     = "DevOpsTeam"
       ManagedBy = "Terraform"
     }
