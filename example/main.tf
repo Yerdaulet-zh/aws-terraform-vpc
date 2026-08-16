0131fcfadb510d2c31825190e2f1bee22ac762d8
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-08-16 15:34:16 +0500

    KAN-1: at example.main file omit the default params

--- example/main.tf
+++ example/main.tf
@@ -2,14 +2,7 @@ module "vpc_test" {
   source = "../"
 
   vpc_config = {
-    ipv4_ipam_id                         = null
-    ipv4_netmask_length                  = null
-    ipv4_cidr_block                      = "10.100.0.0/16"
-    assign_generated_ipv6_cidr_block     = true
-    instance_tenancy                     = "default"
-    enable_dns_support                   = true
-    enable_network_address_usage_metrics = true
-    enable_dns_hostnames                 = true
+    ipv4_cidr_block = "10.100.0.0/16"
     tags = {
       Owner     = "DevOpsTeam"
       ManagedBy = "Terraform"
