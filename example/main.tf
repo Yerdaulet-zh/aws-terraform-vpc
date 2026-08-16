3c69c3684ccc4c924673a310071b94f34ca5fc6f
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-08-16 16:38:09 +0500

    KAN-2: check if validation on cidr and ipam of IPv4 fails -- confirming it works, resetting back the code chanes needed to test the validator.

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,7 @@ module "vpc_test" {
   source = "../"
 
   vpc_config = {
-    # ipv4_cidr_block                  = "10.100.0.0/16"
+    ipv4_cidr_block                  = "10.100.0.0/16"
     assign_generated_ipv6_cidr_block = true
     tags = {
       Name      = "vpc-module-example"
