064a0fa85e20150e679d0accd6fc4c0a78884ebe
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-08-16 16:20:20 +0500

    KAN-2: remove un-needed default values from vpc_config. add assign_generated_ipv6_cidr_block optional parameter.

--- variables.tf
+++ variables.tf
@@ -1,10 +1,14 @@
 variable "vpc_config" {
   description = "Default configuration for the VPC"
   type = object({
-    ipv4_ipam_id                         = optional(string)
-    ipv4_netmask_length                  = optional(number)
-    ipv4_cidr_block                      = optional(string, "10.0.0.0/16")
-    assign_generated_ipv6_cidr_block     = optional(bool, true)
+    ipv4_ipam_id        = optional(string)
+    ipv4_netmask_length = optional(number)
+    ipv4_cidr_block     = optional(string)
+
+    ipv6_ipam_pool_id                = optional(string)
+    ipv6_netmask_length              = optional(number)
+    assign_generated_ipv6_cidr_block = optional(bool)
+
     instance_tenancy                     = optional(string, "default")
     enable_dns_support                   = optional(bool, true)
     enable_network_address_usage_metrics = optional(bool, true)
