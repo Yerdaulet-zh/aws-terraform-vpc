cde04bc3f968da89a9cbc5ead6c4e31f84d6fd34
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-08-16 16:25:55 +0500

    KAN-2: add assign_generated_ipv6_cidr_block, but let VPC be ipv4 only.

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,15 @@
 resource "aws_vpc" "main" {
-  # If the key isn't in the map, return null
-  ipv4_ipam_pool_id   = lookup(local.vpc_config, "ipv4_ipam_pool_id", null)
-  ipv4_netmask_length = lookup(local.vpc_config, "ipv4_netmask_length", null)
-  cidr_block          = lookup(local.vpc_config, "cidr_block", null)
+  # IPv4 Configuration
+  cidr_block          = var.vpc_config.ipv4_ipam_id == null ? var.vpc_config.ipv4_cidr_block : null
+  ipv4_ipam_pool_id   = var.vpc_config.ipv4_ipam_id
+  ipv4_netmask_length = var.vpc_config.ipv4_ipam_id != null ? var.vpc_config.ipv4_netmask_length : null
 
+  # IPv6 Configuration
+  ipv6_ipam_pool_id                = var.vpc_config.ipv6_ipam_pool_id
+  ipv6_netmask_length              = var.vpc_config.ipv6_ipam_pool_id != null ? var.vpc_config.ipv6_netmask_length : null
+  assign_generated_ipv6_cidr_block = var.vpc_config.ipv6_ipam_pool_id == null ? var.vpc_config.assign_generated_ipv6_cidr_block : null
+
+  # General Attributes
   instance_tenancy                     = var.vpc_config.instance_tenancy
   enable_dns_support                   = var.vpc_config.enable_dns_support
   enable_network_address_usage_metrics = var.vpc_config.enable_network_address_usage_metrics
