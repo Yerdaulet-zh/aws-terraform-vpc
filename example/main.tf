6f1b3609e607c70b4d1e1712b45ec73d221e3b87
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-08-21 09:11:23 +0500

    KAN-2: add a tag for example VPC

--- example/main.tf
+++ example/main.tf
@@ -10,6 +10,9 @@ module "vpc_test" {
   vpc_config = {
     ipv4_cidr_block                  = "10.100.0.0/16"
     assign_generated_ipv6_cidr_block = true
+    tags = {
+      Name = "Dual-Stack Example VPC"
+    }
   }
 
   subnets = {
