732d2e16a75e0a4d9bb81ce11edad527e4a1c95a
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-08-21 16:09:44 +0500

    KAN-2: add private route table with EOIGW route for private IPv6 and Dual-Stack subnets, aws_route_table_association also included in the file.

---
+++ route_table_private.tf
@@ -0,0 +1,20 @@
+resource "aws_route_table" "private" {
+  count  = length(local.private_subnets) > 0 ? 1 : 0
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(local.default_tags, { Name = "${var.global.project_name}-private-rt" })
+}
+
+# IPv6 Outbound-Only Egress via EOIGW
+resource "aws_route" "private_eoigw" {
+  count                       = local.has_ipv6_private && length(aws_egress_only_internet_gateway.this) > 0 ? 1 : 0
+  route_table_id              = aws_route_table.private[0].id
+  destination_ipv6_cidr_block = "::/0"
+  egress_only_gateway_id      = aws_egress_only_internet_gateway.this[0].id
+}
+
+resource "aws_route_table_association" "private" {
+  for_each       = local.private_subnets
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private[0].id
+}
