f04a26c2e9fe93f48c57dab7f88e369be12bb9ea
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-08-21 16:17:04 +0500

    KAN-2: the private subnets split into two in locals, added new params one holds private ipv6 compatible subnets, another one which hold only IPv4 compatible subnets

--- locals.tf
+++ locals.tf
@@ -12,9 +12,20 @@ locals {
     if try(v.map_public_ip_on_launch, false) || try(v.assign_ipv6_address_on_creation, false)
   }
 
-  private_subnets = {
+  all_private_subnets = {
     for k, v in aws_subnet.subnets : k => v
     if !contains(keys(local.public_subnets), k)
+  }
+
+  # Split Private Subnets by IPv6 Capability such as IPv6 Native or IPv6 Enabled (Dual-Stack) Subnets
+  private_ipv6_subnets = {
+    for k, v in local.all_private_subnets : k => v
+    if v.ipv6_cidr_block != null
+  }
+
+  private_ipv4_only_subnets = {
+    for k, v in local.all_private_subnets : k => v
+    if v.ipv6_cidr_block == null
   }
 
   # Determine if there are any public IPv4 or IPv6 subnets configured by the user.
@@ -23,5 +34,5 @@ locals {
   # See: aws_route_table --- count  = length(local.public_subnets) > 0 ? 1 : 0
   has_ipv4_public  = length([for s in local.public_subnets : s if s.cidr_block != null]) > 0
   has_ipv6_public  = length([for s in local.public_subnets : s if s.ipv6_cidr_block != null]) > 0
-  has_ipv6_private = length([for s in local.private_subnets : s if s.ipv6_cidr_block != null]) > 0
+  has_ipv6_private = length(local.private_ipv6_subnets) > 0
 }
