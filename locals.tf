13c55cdac679ed86d32f990a1376da640db6bad7
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-08-21 16:07:53 +0500

    KAN-2: add local params which Automatically Categorize Subnets that used by IGW, EOIGW, Route Tables, and NAT Gateways

--- locals.tf
+++ locals.tf
@@ -5,4 +5,23 @@ locals {
     Owner       = "Terraform"
     ManagedBy   = "DevOpsTeam"
   }
+
+  # Automatically Categorize Subnets which used by IGW, EOIGW, Route Tables, and NAT Gateways
+  public_subnets = {
+    for k, v in aws_subnet.subnets : k => v
+    if try(v.map_public_ip_on_launch, false) || try(v.assign_ipv6_address_on_creation, false)
+  }
+
+  private_subnets = {
+    for k, v in aws_subnet.subnets : k => v
+    if !contains(keys(local.public_subnets), k)
+  }
+
+  # Determine if there are any public IPv4 or IPv6 subnets configured by the user.
+  # If no public subnets are configured for IPv4, then route table association will not be created for IPv4, and vise versa.
+  # If no public subnets are configured for either IPv4 or IPv6, then the public route table will not be created at all.
+  # See: aws_route_table --- count  = length(local.public_subnets) > 0 ? 1 : 0
+  has_ipv4_public  = length([for s in local.public_subnets : s if s.cidr_block != null]) > 0
+  has_ipv6_public  = length([for s in local.public_subnets : s if s.ipv6_cidr_block != null]) > 0
+  has_ipv6_private = length([for s in local.private_subnets : s if s.ipv6_cidr_block != null]) > 0
 }
